539f2a783a6ade75b90f527fd5d4245facb0ef80
Ja Tar <tarnogorskijan@gmail.com>
2025-12-11 22:09:08 +0100

    theme fixes

--- src/view.js
+++ src/view.js
@@ -149,27 +149,44 @@ function setupTheme() {
 
     if (!themeOverride) {
         if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            document.documentElement.dataset["theme"] = "dark";
+            document.documentElement.dataset.theme = "dark";
+            themeOverride = "dark";
         } else {
-            document.documentElement.dataset["theme"] = "light";
+            document.documentElement.dataset.theme = "light";
+            themeOverride = "light";
         }
     } else {
-        document.documentElement.dataset["theme"] = themeOverride;
+        document.documentElement.dataset.theme = themeOverride;
     }
+    updateAppBackground(themeOverride);
 }
 
 function toggleTheme() {
     let themeOverride = localStorage.getItem("theme-override");
 
-    if (document.documentElement.dataset["theme"] === "dark") {
-        document.documentElement.dataset["theme"] = "light";
+    if (document.documentElement.dataset.theme === "dark") {
+        document.documentElement.dataset.theme = "light";
         themeOverride = "light";
     } else {
-        document.documentElement.dataset["theme"] = "dark";
+        document.documentElement.dataset.theme = "dark";
         themeOverride = "dark";
     }
 
     localStorage.setItem("theme-override", themeOverride);
+    updateAppBackground(themeOverride);
+}
+
+/**
+ * @param {string} themeOverride theme dataset string
+ */
+function updateAppBackground(themeOverride) {
+    const element = document.querySelector("meta[name='theme-color'");
+    console.log(themeOverride, element);
+    let color = "#fff";
+    if (themeOverride === "dark") {
+        color = "#151515";
+    }
+    element.setAttribute("content", color);
 }
 
 document.getElementById("theme-button").addEventListener("click", toggleTheme);
